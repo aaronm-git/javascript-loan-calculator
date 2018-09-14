18f35b7f8a7b5651758c673c65b1578051513134
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2018-09-14 15:40:48 -0400

    latest commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,31 +9,44 @@ $(document).ready(function () {
         var selectedInterval = $('#selectedInterval').val();
         var installmentsNum = $('#installmentsNum').val();
 
-        if (selectedInterval == "daily") {
-            for (var i = 1, newEndingBalance = loanAmount; i <= installmentsNum; i++) {
-                var newDate = new Date(startDate);
+        for (var i = 1; i <= installmentsNum; i++) {
+            newDate = new Date(startDate);
+            if (selectedInterval == 'daily') {
                 newDate.setDate(newDate.getDate() + i);
-
-                var dd = newDate.getDate();
-                var mm = newDate.getMonth();
-                var y = newDate.getFullYear();
-                var newDate = mm + '/' + dd + '/' + y;
-                console.log(newDate);
-
-                var scheduledPayment = Math.ceil(((loanAmount * interestRate) / installmentsNum) * 100) / 100;
-                console.log(scheduledPayment);
-
-                newEndingBalance = loanAmount - (scheduledPayment*i);
-                $("#result-table tbody:last-child")
-                    .append(
-                        '<tr><td scope="row">' + i + '</td>' +
-                        '<td>' + newDate + '</td>' +
-                        '<td>$' + newEndingBalance + '</td>' +
-                        '<td>$' + scheduledPayment + '</td>' +
-                        '<td>' + interestRate + '%</td>' +
-                        '<td>$' + newEndingBalance + '</td>' +
-                        '</tr>');
             }
+
+            else if (selectedInterval == 'weekly') {
+                newDate.setDate(newDate.getDate() + (i * 7));
+            }
+
+            else if (selectedInterval == 'monthly') {
+                newDate.setMonth(newDate.getMonth() + i);
+            }
+
+            var dd = newDate.getDate();
+            var mm = newDate.getMonth() + 1;
+            var y = newDate.getFullYear();
+            var newDate = mm + '/' + dd + '/' + y;
+
+            var scheduledPayment = currency(loanAmount).distribute(installmentsNum);
+            if (interestRate != 0){
+                scheduledPayment[i-1] = currency(scheduledPayment[i-1]).add(currency(scheduledPayment[i-1]).multiply(interestRate));
+            }
+
+            var currentBalance = loanAmount - (currency(scheduledPayment[i-1]*i)-scheduledPayment[i-1]);
+            var newEndingBalance = loanAmount - (currency(scheduledPayment[i-1]).multiply(i));
+
+            // var currentBalance = currency(loanAmount).subtract(currency(currency(scheduledPayment[i-1]).multiply(i)).add(scheduledPayment[i-1]));
+            // var newEndingBalance = currency(loanAmount).subtract(currency(scheduledPayment[i-1]).multiply(i));
+            $("#result-table tbody:last-child")
+                .append(
+                    '<tr><td scope="row">' + i + '</td>' +
+                    '<td>' + newDate + '</td>' +
+                    '<td>$' + currentBalance.toFixed(2) + '</td>' +
+                    '<td>$' + scheduledPayment[i-1] + '</td>' +
+                    '<td>' + interestRate + '%</td>' +
+                    '<td>$' + newEndingBalance.toFixed(2) + '</td>' +
+                    '</tr>');
         }
     });
 });
